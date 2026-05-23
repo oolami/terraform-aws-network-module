d566252fced47cd2f977046f6178b65fbe49b0a4
root <root@example.com>
2026-05-23 07:02:23 -0400

    Initial Terraform AWS network module

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+module "network" {
+  source = "./modules/network"
+
+  project_name         = var.project_name
+  vpc_cidr             = var.vpc_cidr
+  public_subnet_cidrs  = var.public_subnet_cidrs
+  private_subnet_cidrs = var.private_subnet_cidrs
+  availability_zones   = var.availability_zones
+}
---
+++ modules/network/variables.tf
@@ -0,0 +1,5 @@
+variable "project_name" {}
+variable "vpc_cidr" {}
+variable "public_subnet_cidrs" {}
+variable "private_subnet_cidrs" {}
+variable "availability_zones" {}
