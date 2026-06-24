0d763ac270b7a6cdd8f255c7662ee3f22776ecb8
root <root@example.com>
2026-06-24 07:24:31 -0400

    updated public subnet

--- main.tf
+++ main.tf
@@ -11,3 +11,7 @@ module "network" {
   private_subnet_cidrs = var.private_subnet_cidrs
   availability_zones   = var.availability_zones
 }
+
+public_subnet_cidrs = [
+  "10.0.1.0/24",
+  "10.0.2.0/24"
